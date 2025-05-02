3eb052af249208d4e15bc50648449698dcb03286
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-05-02 17:31:23 -0300

    feat: footer component

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2025
+            <a href="https://github.com/GB-Saraiva" target="_blank"> Saraiva</a>
+        </strong>
+    </footer>
+)
