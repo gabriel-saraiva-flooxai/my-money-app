fa8a63f1e63bbbe7dd314173bc70934cac0916e9
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-05-02 14:13:24 -0300

    feat: middleware query parser int

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,12 +2,14 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 const port = 3005
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function() {
     console.log(`\n\n\n...BACKEND is running on port ${port}...\n\n\n`);
