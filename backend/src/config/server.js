66f57a1338679af4277dd86d25b69e50301d0327
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-05-02 14:02:58 -0300

    chore: middleware to enable CORS

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,11 +1,13 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 const port = 3005
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
     console.log(`\n\n\n...BACKEND is running on port ${port}...\n\n\n`);
