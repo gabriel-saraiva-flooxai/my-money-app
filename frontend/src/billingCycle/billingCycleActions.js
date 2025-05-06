404315e4ec41542918effbf3cecdd7ca2aa8d8f4
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-05-06 17:42:57 -0300

    feat: implement credit management in billing cycle form

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -4,7 +4,7 @@ import { reset, resetForm, initialize } from "redux-form";
 import { showTabs, selectTab } from '../common/tab/tabActions';
 
 const BASE_URL = 'http://localhost:3005/api'
-const INITIAL_VALUES = {}
+const INITIAL_VALUES = {credits: [{}]}
 
 export function getList() {
     const request = axios.get(`${BASE_URL}/billingCycles`)
